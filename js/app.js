c6ce86e3d5fa5bdaaeb100aa57031db30312159a
Kyle <kyleludlow@gmail.com>
2016-04-19 11:01:32 -0700

    gets thumbnail and title information from youtube API and displays below search field

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    $("ul").css("list-style-type", "none");
+
     $("#searchTerm").submit(function(event){
         event.preventDefault();
         var query = $('#query').val();
@@ -7,23 +9,37 @@ $(function() {
     });
     function getRequest(query) {
         var params = {
-            key: "AIzaSyBcohE5KMlFccLNp_ZnMDI-bRcbJDHMgDUAIzaSyBcohE5KMlFccLNp_ZnMDI-bRcbJDHMgDU",
-            part: 'snippet',
+            key: 'AIzaSyBcohE5KMlFccLNp_ZnMDI-bRcbJDHMgDU',
+            part: 'id, snippet',
             q: query
         };
         url = 'https://www.googleapis.com/youtube/v3/search';
 
         $.getJSON(url, params, function(data) {
-            showResults(data.Search);
+            console.log(data.items[2].snippet.thumbnails.default.url);
+
+            showResults(data.items);
         });
     }
 
     function showResults(results) {
         var html = "";
         $.each(results, function(index, value) {
-            html += '<li>' + value.Title + '</li>';
-            console.log(value.Title);
+            var thumbnail = value.snippet.thumbnails.default.url;
+            html += "<li><img src='" + thumbnail + "'>" + value.snippet.title + "</li>";
+            console.log(value.snippet.title);
         });
         $('#queryResults').html(html);
     }
 });
+
+
+
+//this is what the $.each method is doing
+/* var html = "";
+ $.each(results, function(index, value) {
+ html += '<li>' + value.snippet.title + '</li>';
+ console.log(value.snippet.title);
+ });
+ $('#queryResults').html(html); */
+//for (var i = 0; i < data.items.length; i++)
